7efaf0edf205364ffc39a8564066b9b48c39f926
Salaton <nairouasalaton@gmail.com>
2022-11-30 01:39:47 +0300

    feat: create more resources

--- main.go
+++ main.go
@@ -3,19 +3,32 @@ package main
 import (
 	bucket "quickstart/buckets"
 	computeengine "quickstart/computeEngine"
+	clouddns "quickstart/dns"
+	service "quickstart/services"
 
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
 
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
+		// First, enable any services
+		_, err := service.EnableService(ctx)
+		if err != nil {
+			return err
+		}
+
 		// Create a GCP resource (Storage Bucket) - Details are hardcoded for now
-		err := bucket.CreateStorageBucket(ctx)
+		err = bucket.CreateStorageBucket(ctx)
 		if err != nil {
 			return err
 		}
 
 		err = computeengine.CreateComputeEngineInstance(ctx)
+		if err != nil {
+			return err
+		}
+
+		err = clouddns.CreateManagedZone(ctx)
 		if err != nil {
 			return err
 		}
